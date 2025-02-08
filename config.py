17f8bf15fad22fa09e156f2bc526603b36306b23
gamingvikash649 <gamingvikash649@gmail.com>
2025-02-08 20:49:10 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,5 @@
+BOT_NAME = "Moni Uploader 2"
+API_ID    = 20838202
+API_HASH  = "fde3e78e3256ae13e868b39088c83838"
+BOT_TOKEN = "7685126120:AAEG-s59bY_CU5weOeUNwRz1CXpO2_HYrU0"
+MONGO_URI = "mongodb+srv://vk0634912:OKMino2da3dSzlZB@cluster0.pdtg3.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
