fe355cee4117fa8efea57e3a199df6fd698a864a
gamingvikash649 <gamingvikash649@gmail.com>
2025-02-08 20:16:20 +0530

    Update config.py

--- modules/config.py
+++ modules/config.py
@@ -1,5 +1,5 @@
 BOT_NAME = "Moni Uploader 2"
-API_ID    = 24495656
-API_HASH  = "61afcf68c6429714dd18acd07f246571"
-BOT_TOKEN = "8040014845:AAHAo3zgpikp-4j2PLQofMxSwtoOdQKTQLg"
-MONGO_URI = "mongodb+srv://mrnobody:modernhackers@mrnobody.q8e87ij.mongodb.net/?retryWrites=true&w=majority&appName=MrNobody"
+API_ID    = 20838202
+API_HASH  = "fde3e78e3256ae13e868b39088c83838"
+BOT_TOKEN = "7685126120:AAEG-s59bY_CU5weOeUNwRz1CXpO2_HYrU0"
+MONGO_URI = "mongodb+srv://vk0634912:OKMino2da3dSzlZB@cluster0.pdtg3.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
